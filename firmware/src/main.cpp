19b8262e50ef6483080ca223e60355d0aea15e60
John Zastrow <br8kwall@gmail.com>
2025-12-01 08:22:36 -0500

    commented line for other sensors

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -22,7 +22,7 @@
 #include <Preferences.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include <Adafruit_BME280.h>  // change for different sensors
 #include <esp_task_wdt.h>
 #include <esp_ota_ops.h>
 
